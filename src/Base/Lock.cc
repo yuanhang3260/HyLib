e368dc8984006019acd4915518e69193bb967f02
Hang Yuan <yuanhang3260@gmail.com>
2017-05-24 18:44:02 -0700

    Read-Write lock added

--- src/Base/Lock.cc
+++ src/Base/Lock.cc
@@ -2,6 +2,40 @@
 
 namespace base {
 
+void RWLock::ReadLock() {
+  std::unique_lock<std::mutex> lock(mutex_);
+  reader_cv_.wait(lock, [&] { return waiting_writers_ == 0 &&
+                                     active_writers_ == 0; });
+  active_readers_++;
+}
 
+void RWLock::ReadUnlock() {
+  std::unique_lock<std::mutex> lock(mutex_);
+  active_readers_--;
+  if (active_readers_ == 0) {
+    writer_cv_.notify_one();
+  }
+}
+
+void RWLock::WriteLock() {
+  std::unique_lock<std::mutex> lock(mutex_);
+  waiting_writers_++;
+  writer_cv_.wait(lock, [&] { return active_readers_ == 0 &&
+                                     active_writers_ == 0; });
+  waiting_writers_--;
+  active_writers_++;
+}
+
+void RWLock::WriteUnlock() {
+  std::unique_lock<std::mutex> lock(mutex_);
+  waiting_writers_--;
+  active_writers_--;
+  if (waiting_writers_ > 0) {
+    writer_cv_.notify_one();
+  }
+  else {
+    reader_cv_.notify_all();
+  }
+}
 
 }  // namespace base
--- src/Base/Lock.h
+++ src/Base/Lock.h
@@ -2,15 +2,15 @@
 #define BASE_LOCK_
 
 #include <atomic>
+#include <mutex>
+#include <condition_variable>
+
+#include "Base/MacroUtils.h"
 
 namespace base {
 
 class SpinLock {
  public:
-  ~SpinLock() {
-    unlock();
-  }
-
   void lock() {
     while (locked.test_and_set(std::memory_order_acquire)) {}
   }
@@ -21,6 +21,30 @@ class SpinLock {
 
  private:
   std::atomic_flag locked = ATOMIC_FLAG_INIT;
+
+  FORBID_COPY_AND_ASSIGN(SpinLock);
+};
+
+// This is a writer-favored read-write lock. Readers can be starved.
+class RWLock {
+ public:
+  RWLock() = default;
+  ~RWLock();
+
+  void ReadLock();
+  void ReadUnlock();
+  void WriteLock();
+  void WriteUnlock();
+
+ private:
+  std::mutex mutex_;
+  std::condition_variable reader_cv_;
+  std::condition_variable writer_cv_;
+  int active_readers_ = 0;
+  int waiting_writers_ = 0;
+  int active_writers_ = 0;
+
+  FORBID_COPY_AND_ASSIGN(RWLock);
 };
 
 }  // namespace base
